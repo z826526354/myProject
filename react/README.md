f259ce1061cc201ca882c1d458d91c6f1e7fecbb
Tommy Chen <tommy351@gmail.com>
2019-10-20 17:04:13 +0800

    react-demo

--- react/README.md
+++ react/README.md
@@ -2,3 +2,5 @@
 
 ### [探究react的MVVM模式](./react.md)
 
+#### [reacct 项目:日记便签](https://z826526354.github.io/myProject/react/build/index.html)
+
---
+++ react/build/precache-manifest.a8df7612e07077c52d36b58083535be3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1240ec50598422db025f07a340f8dc62",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6bf673881afd6b4a302c",
+    "url": "./static/css/main.dc4be281.chunk.css"
+  },
+  {
+    "revision": "27ac806c9657d36d63f1",
+    "url": "./static/js/2.6610ab08.chunk.js"
+  },
+  {
+    "revision": "6bf673881afd6b4a302c",
+    "url": "./static/js/main.4f75c9eb.chunk.js"
+  },
+  {
+    "revision": "9adf26229241a35e80e1",
+    "url": "./static/js/runtime-main.51c2ac1d.js"
+  }
+]);
